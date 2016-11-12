fd5339ef6eafe7f6cf579dfff22c6cc29fbf7e37
Bonnie Xu <bonniexu@Bonnies-MacBook-Pro.local>
2016-11-12 08:14:51 -0500

    Minor changes to home

--- server/apis.js
+++ server/apis.js
@@ -1,20 +1,57 @@
-var router = require('express').Router();
-var path = require('path');
+let router = require('express').Router();
+let path = require('path');
+let axios = require('axios')
+let parseString = require('xml2js').parseString
+let _ = require('lodash');
+let simulate = require('./simulation/simulate'); 
+
+const NASDAQ_TOKEN = 'BC2B181CF93B441D8C6342120EB0C971'
+
+const TRADES_URL = 'http://ws.nasdaqdod.com/v1/NASDAQAnalytics.asmx/GetSummarizedTrades'
 
 router.get("/stocks", function (req, res) {
 	res.sendFile(path.join(__dirname, "/stocks.json"))
 })
 
 router.post("/algorithmSimulation", function (req, res) {
-	var algo_data = {
-		ticket: req.body.ticket,
-		startTime: req.body.startTime,
-		endTime: req.body.endTime,
-		startingMoney: req.body.startingMoney
-	}
-
-	res.json(algo_data)
-	
+  let symbols = ''
+  console.log(req.body.stocks)
+  req.body.stocks.forEach(function(s) {
+    symbols = symbols + (s.ticket + ",")
+  })
+  console.log("SYMBOLS IS :" , symbols)
+	axios.get(TRADES_URL, {
+		params: {
+      '_Token' : NASDAQ_TOKEN,
+      'Symbols' : symbols,
+      'StartDateTime' : req.body.startTime + " 09:30:00",
+      'EndDateTime' : req.body.endTime + " 16:00:00",
+      'MarketCenters' : '' ,
+      'TradePrecision': 'Hour',
+      'TradePeriod':'1'
+    }
+	})
+  .then(function (response) {
+    parseString(response.data, function (err, json) {
+      if (err) {
+        res.json({Error: "Nasdaq api shit the bed"})
+      } else {
+        //console.log("gdfgd")
+        json = json["ArrayOfSummarizedTradeCollection"]["SummarizedTradeCollection"];
+        json = _.map(json, function(item){
+          let val = {};
+          val.trades = item["SummarizedTrades"][0]["SummarizedTrade"];
+          val.ticker = item["Symbol"];
+          return val;
+        });
+        var val = simulate(req.body.code, req.body.stocks, json, req.body.startingCash);
+        res.send(val);
+      }
+    });
+  })
+  .catch(function (error) {
+    res.json(error)
+  });
 })
 
 module.exports = router
--- server/simulation/simulate.js
+++ server/simulation/simulate.js
@@ -4,12 +4,12 @@ module.exports = function(code, stocks, tradingData, startingCash){
   var trader = new Trader(startingCash);
   // Creates the stocks
   for (let idx in stocks){
-    var stock = stock[idx];
-    trader.addStock(new Stock(trader, "TICKER HERE", "NAME HERE"));
+    var stock = stocks[idx];
+    trader.addStock(new Stock(trader, stock.ticket, stock.company));
   }
   // This expects the code to set the variable stockUpdate (i.e. AlgoTrader.stockUpdate = function(stock){ ) ...
   var AlgoTrader = trader;
   eval("(function(){" + code + "}())");
   // Runs through the tradingData and simulates it
-  console.log(AlgoTrader.stockUpdate(""));
+  return AlgoTrader.stockUpdate("");
 }
