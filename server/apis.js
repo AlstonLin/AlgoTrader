6498452ec769de6cd71eb19fd5aeebb71484182e
utk9 <utkarshlamba.2@gmail.com>
2016-11-12 02:39:46 -0500

    Pull data from Nasdaq api

--- server/apis.js
+++ server/apis.js
@@ -1,5 +1,11 @@
 var router = require('express').Router();
 var path = require('path');
+let axios = require('axios')
+var parseString = require('xml2js').parseString
+
+const NASDAQ_TOKEN = 'BC2B181CF93B441D8C6342120EB0C971'
+
+const TRADES_URL = 'http://ws.nasdaqdod.com/v1/NASDAQAnalytics.asmx/GetSummarizedTrades'
 
 router.get("/stocks", function (req, res) {
 	res.sendFile(path.join(__dirname, "/stocks.json"))
@@ -13,7 +19,30 @@ router.post("/algorithmSimulation", function (req, res) {
 		startingMoney: req.body.startingMoney
 	}
 
-	res.json(algo_data)
+	axios.get(TRADES_URL, {
+		params: {'_Token' : NASDAQ_TOKEN,
+          'Symbols' : req.body.ticket,
+          'StartDateTime' : req.body.startTime + " 09:30:00",
+          'EndDateTime' : req.body.endTime + " 16:00:00",
+          'MarketCenters' : '' ,
+          'TradePrecision': 'Hour',
+          'TradePeriod':'1'
+      }
+	})
+  .then(function (response) {
+    console.log(response.data);
+	parseString(response.data, function (err, result) {
+	    if (err) {
+	    	res.json({Error: "Nasdaq api shit the bed"})
+	    } else {
+	    	res.send(result)
+	    }
+	});
+  })
+  .catch(function (error) {
+    res.json(error)
+  });
+
 	
 })
 
--- server/simulation/simulate.js
+++ server/simulation/simulate.js
@@ -5,7 +5,7 @@ module.exports = function(code, stocks, tradingData, startingCash){
   // Creates the stocks
   for (let idx in stocks){
     var stock = stock[idx];
-    trader.addStock(new Stock(trader, "TICKER HERE", "NAME HERE"));
+    trader.addStock(new Stock(trader, stocks.company, stocks.ticket));
   }
   // This expects the code to set the variable stockUpdate (i.e. AlgoTrader.stockUpdate = function(stock){ ) ...
   var AlgoTrader = trader;
