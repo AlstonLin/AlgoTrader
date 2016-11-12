c58f9d80e7112ddaf000f00f9918e6011bf92cca
Alston Lin <alstonlin812@gmail.com>
2016-11-11 23:11:48 -0500

    Added root path file serve

--- server/app.js
+++ server/app.js
@@ -1,11 +1,12 @@
 var PORT = 8080;
 
 var express = require('express');
-
+var path = require('path');
 var app = express();
 
 // TODO: Finish this endpoint to serve the main page
 app.get("/", function(req, res){
+  res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
 
 app.get("/test", function(req, res){
