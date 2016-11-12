8e3ac3693ccce6db65081db4ae403a93c44ec22c
utk9 <utkarshlamba.2@gmail.com>
2016-11-12 01:08:36 -0500

    Add all companies and alogrithm simulator endpoint

--- server/apis.js
+++ server/apis.js
@@ -1,8 +1,20 @@
 var router = require('express').Router();
+var path = require('path');
 
+router.get("/stocks", function (req, res) {
+	res.sendFile(path.join(__dirname, "/stocks.json"))
+})
 
-router.get("/testapi", function (req, res) {
-	res.send("test api successful")
+router.post("/algorithmSimulation", function (req, res) {
+	var algo_data = {
+		ticket: req.body.ticket,
+		startTime: req.body.startTime,
+		endTime: req.body.endTime,
+		startingMoney: req.body.startingMoney
+	}
+
+	res.json(algo_data)
+	
 })
 
 module.exports = router
